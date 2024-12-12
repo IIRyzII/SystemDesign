72a22fa185425343c20b63496f9a20e65768b4cb
IIRyzII <112952727+IIRyzII@users.noreply.github.com>
2024-12-12 00:56:59 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request,redirect, url_for, session
+from flask_session import Session
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+users = {Ryz}
