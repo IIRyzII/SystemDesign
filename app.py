20e1878c1cc593ac73e57a4c284abb356f43550d
IIRyzII <112952727+IIRyzII@users.noreply.github.com>
2024-12-12 10:45:01 +0000

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request,redirect, url_for, session
-from flask_session import Session
-
-app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-users = {Ryz}
